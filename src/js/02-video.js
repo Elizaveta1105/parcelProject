c6ec332970b6210b24f7084357c7bbef9ce86f02
Elizaveta <elizaveta@short.io>
2022-09-05 18:29:51 +0300

    video task added

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
+import Player from '@vimeo/player';
+import { throttle } from 'lodash'
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const VIDEO_PLAYER_STORAGE = "videoplayer-current-time"
+
+player.on('timeupdate', throttle(videoStop, 1000))
+
+if(localStorage.getItem(VIDEO_PLAYER_STORAGE)) {
+    player.setCurrentTime(localStorage.getItem(VIDEO_PLAYER_STORAGE))
+}
+
+function videoStop(event) {
+    localStorage.setItem(VIDEO_PLAYER_STORAGE, event.seconds)
+}
