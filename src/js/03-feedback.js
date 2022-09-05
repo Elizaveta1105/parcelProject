cf54031c5879a016fad35fc56276f1eef118de12
Elizaveta <elizaveta@short.io>
2022-09-05 19:48:40 +0300

    added throttle

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,17 +1,18 @@
+import { throttle } from 'lodash'
 const emailInput = document.querySelector("[name='email']")
 const messageInput = document.querySelector("[name='message']")
 const form = document.querySelector(".feedback-form")
 const submitButton = document.querySelector("[type='submit']")
 const FEEDBACK_STORAGE = "feedback-form-state"
 
-form.addEventListener("input", () => {
+form.addEventListener("input", throttle(() => {
     const data = {
         email: emailInput.value,
         message: messageInput.value
     }
 
     localStorage.setItem(FEEDBACK_STORAGE, JSON.stringify(data))
-})
+}), 500)
 
 if(localStorage.getItem(FEEDBACK_STORAGE)) {
     let parsedData = JSON.parse(localStorage.getItem(FEEDBACK_STORAGE))
